952465b200b586408947bbea76401daf950c8deb
Carter Walsh <Cwalsh616@gmail.com>
2021-07-19 13:54:56 -0500

    added another greeting test

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test("can create a basic greeting 2", () => {
+    expect(getGreeting("Carter")).toBe("Hi Carter");
+});
